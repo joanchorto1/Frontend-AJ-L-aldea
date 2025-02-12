509617e394f226d0b9235743a52074f9d1e9d05e
alumne <joanchorto1@iesebre.com>
2025-02-12 11:04:13 +0100

    Preparació de l'entorn

---
+++ src/boot/axios.js
@@ -0,0 +1,9 @@
+import { boot } from 'quasar/wrappers';
+import axios from 'axios';
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api'
+});
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = api;
+});
+export { api };
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { api } from 'boot/axios';
+const message = ref('');
+onMounted(async () => {
+  const response = await api.get('/hello');
+  message.value = response.data.message;
+});
+</script>
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-card-section>
+        <q-btn label="Crida API" @click="onMounted" />
+        <p>{{ message }}</p>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
